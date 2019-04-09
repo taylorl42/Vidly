84037df3d15a217cd249c7cb666fba071d1f5807
Taylor <Taylorl@rogers.com>
2019-04-09 11:43:37 -0400

    Added Customers Table

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+
+}
