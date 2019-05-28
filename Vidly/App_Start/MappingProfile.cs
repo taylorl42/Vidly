e898a989184cfdfefb2034d26d89426d0a65e477
Taylor <Taylorl@rogers.com>
2019-05-28 09:11:38 -0400

    Finished setting up Movies Api

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,8 +12,13 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
         }
     }
 }
